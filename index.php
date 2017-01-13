5d552521b4a221078a156758cee434ef5d3c7256
Chris Lam <christopher.lam@dimensiondata.com>
2017-01-13 12:36:29 +0800

    init version

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$localIP = $_SERVER['SERVER_ADDR'];
+$version = "1";
+
+
+echo "<h3>This page is for OpenShift pod upgrade demo.</h3>";
+echo "<table style=\"height: 59px;\" border=\"3\" width=\"294\">";
+echo "<tbody>";
+echo "<tr>";
+echo "<td><strong>Version</strong></td>";
+echo "<td style=\"text-align: center;\"><span style=\"color: #0000ff;\"><strong>$version</strong></span></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><strong>IP</strong></td>";
+echo "<td style=\"text-align: center;\"><span style=\"color: #0000ff;\"><strong>$localIP</strong></span></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><strong>Image</strong></td>";
+echo "<td><center><img src=\"./blue.png\" alt=\"\" width=\"160\" height=\"160\" /></center></td>";
+echo "</tr>";
+echo "</tbody>";
+echo "</table>";
+
+?>
